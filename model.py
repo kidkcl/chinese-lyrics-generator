c90b431069a94d215ee55987a747520890f3fd08
kidkcl <yagamigin@gmail.com>
2018-01-13 15:50:31 +0800

    embedded input bug

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from keras.models import Model
 
 class Autoencoder:
      
-    def __init__(self, latent_dim=256):
+    def __init__(self, latent_dim=64):
         self.encoder = None
         self.autoencoder = None
         self.latent_dim = latent_dim
@@ -11,8 +11,8 @@ class Autoencoder:
 
     def build(self, sequence_length, input_dim):
         inputs = Input(shape=(None,))
-        inputs = Embedding(input_dim, 100, input_length=sequence_length)(inputs)
-        encoded = LSTM(self.latent_dim)(inputs)
+        embedded = Embedding(input_dim, 100, input_length=sequence_length)(inputs)
+        encoded = LSTM(self.latent_dim)(embedded)
 
         decoded = RepeatVector(sequence_length)(encoded)
         decoded = LSTM(input_dim, return_sequences=True)(decoded)
@@ -21,5 +21,6 @@ class Autoencoder:
         encoder = Model(inputs, encoded)
         self.autoencoder = sequence_autoencoder
         self.encoder = encoder
+        return sequence_autoencoder, encoder
 
 
