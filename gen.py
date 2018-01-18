710956c14751c7807e7b9d3a529643ea2656a778
kidkcl <yagamigin@gmail.com>
2018-01-18 14:22:50 +0800

    autoencoder code

--- gen.py
+++ gen.py
@@ -5,8 +5,7 @@ import keras
 from gensim.models import word2vec
 from tqdm import tqdm
 from keras.optimizers import RMSprop
-from keras.callbacks import ModelCheckpoint
-from autoencoder import *
+from model import *
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth=True
@@ -14,46 +13,49 @@ sess = tf.Session(config=config)
 
 keras.backend.set_session(sess)
 
-epochs = 500
+epochs = 200
 batch_size = 50
-sequence_len = 39
+seqence_len = 58
 num_samples = 1000
-input_dim = 100
 
 def main():
     training_data = []
-    with open('pinyin.txt', 'r') as input_file:
+    with open('test1.txt', 'r') as input_file:
         raw_data = input_file.readlines()
         for line in raw_data:
             line = line.strip()
             training_data.append(line.split(' '))
+    #import ipdb; ipdb.set_trace()
     # load pre-trained word2vec model
     wv_model = word2vec.Word2Vec.load('pretrain100.model.bin')
     batch_X = []
-    batch_Y = []
     for i in tqdm(range(len(training_data))):
         sequence_vec = []
         for word in training_data[i]:
             sequence_vec.append(wv_model.wv[word])
         batch_X.append(sequence_vec)
-        if i is not 0:
-            batch_Y.append(sequence_vec)
-    batch_Y.append(batch_X[-1])
     encoder_inputs = np.asarray(batch_X)
-    decoder_outputs = np.asarray(batch_Y)
     print(encoder_inputs.shape)
-    print(decoder_outputs.shape)
     # build model
-    model = Autoencoder(128) # latent_dim
-    model.build(sequence_len, input_dim)
-    print("build model done")
-    model.autoencoder.compile(optimizer='rmsprop', loss='mse')
-    model.autoencoder.summary()
-    filepath = '500epochs/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5'
-    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True)
-    callbacks_list = [checkpoint]
-    model.autoencoder.fit(encoder_inputs, decoder_outputs, epochs=epochs, batch_size=batch_size, callbacks=callbacks_list)
-    model.autoencoder.save('seq_autoencoder.h5')
+    model = keras.models.load_model('seq_autoencoder.h5')
+    #model = keras.models.load_model('test_dim256/weights-improvement-100-0.0579.hdf5')
+    output = model.predict(encoder_inputs)
+    print(output.shape)
+    text = []
+    for i in range(len(output)):
+        for j in range(len(output[i])):
+            wv = output[i][j]
+            word = wv_model.most_similar(positive=[wv], topn=1)
+            #print(word)
+            text.append(word[0][0])
+        text.append('\n')
+
+    print(text)
+    string_text = ' '.join(text)
+    with open("out1_500.txt", 'wb') as out_file:
+        out_file.write(string_text.encode("utf-8"))
+        out_file.close()
+    
 
 if __name__ == "__main__":
     main()
